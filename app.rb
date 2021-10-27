69f33adbdfcaaec247164f5e65ac33d725bbcb06
Nga <38340962+nga20167304@users.noreply.github.com>
2021-10-27 22:43:12 +0700

    change save data json file from array to hash

--- app.rb
+++ app.rb
@@ -3,30 +3,26 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
+require 'byebug'
 
 #
 # ------------ global variable -----------
 #
 
-$json_file_path = 'storage/memo.json'
-$json_data = File.open($json_file_path) do |io|
+json_file_path = 'storage/memo.json'
+json_data = File.open(json_file_path) do |io|
   JSON.parse(io.read)
 end
 
-memos = $json_data['memos']
+memos = json_data
 
 # rewrite the json_data
-def rewrite_json
+def rewrite_json(my_hash)
   File.open('storage/memo.json', 'w') do |file|
-    JSON.dump($json_data, file)
+    file.write(JSON.pretty_generate(my_hash))
   end
 end
 
-#
-# ----------- routing -----------
-#
-
-# root
 get '/' do
   @title = 'main'
   @memos = memos
@@ -42,7 +38,8 @@ end
 # show memo contents
 get '/memos/:id' do
   @title = 'メモを表示'
-  @memo = memos.select { |memo| memo['id'].to_s == params[:id].to_s }
+
+  @memo = memos[params[:id].to_s]
   erb :show
 end
 
@@ -51,17 +48,15 @@ post '/memos' do
   @title = 'メモを追加'
 
   # create id for new memo_data
-  last_id = 0
-  memos.each do |memo|
-    last_id = memo['id'].to_i + 1 if last_id <= memo['id'].to_i
-  end
+  key_arr = memos.keys
+  last_id = key_arr.max.to_i + 1
 
   # create new json data with appended new memo
-  new_memo = { 'id' => last_id.to_s, 'title' => params[:title], 'body' => params[:body] }
+  new_memo = { last_id.to_s => { 'title' => params[:title], 'body' => params[:body] } }
 
-  $json_data['memos'].push(new_memo)
+  json_data = json_data.merge(new_memo)
 
-  rewrite_json
+  rewrite_json(json_data)
 
   redirect '/'
   erb :show
@@ -71,16 +66,9 @@ end
 delete '/memos/:id' do
   @title = 'メモを削除'
 
-  num = 0
-  memos.each do |memo|
-    if memo['id'].to_s == params[:id].to_s
-      $json_data['memos'].delete_at(num)
-      break
-    end
-    num += 1
-  end
+  memos.delete(params[:id].to_s)
 
-  rewrite_json
+  rewrite_json(json_data)
 
   redirect '/'
   erb :index
@@ -89,27 +77,19 @@ end
 # to editer page.
 get '/memos/:id/edit' do
   @title = 'メモを修正'
-  @memo = memos.select { |memo| memo['id'].to_s == params[:id].to_s }
+  @memo = memos[params[:id].to_s]
   erb :edit
 end
 
 # editing process
 patch '/memos/:id' do
   @title = 'メモを修正'
-  new_memo = { 'id' => params[:id].to_s, 'title' => params[:title], 'body' => params[:body] }
 
-  # create new data
-  num = 0
-  memos.each do |memo|
-    if memo['id'].to_s == params[:id].to_s
-      $json_data['memos'][num]['title'] = new_memo['title']
-      $json_data['memos'][num]['body'] = new_memo['body']
-      break
-    end
-    num += 1
-  end
+  edit_memo = memos[params[:id].to_s]
+  edit_memo['title'] = params[:title]
+  edit_memo['body'] = params[:body]
 
-  rewrite_json
+  rewrite_json(json_data)
 
   redirect '/'
   erb :index
@@ -118,9 +98,5 @@ end
 helpers do
   def h(text)
     Rack::Utils.escape_html(text)
-  end
-
-  def hattr(text)
-    Rack::Utils.escape_path(text)
   end
 end
