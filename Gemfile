e4dd5616fca32973bd6cb9b05d73aebb4323824d
Nga <38340962+nga20167304@users.noreply.github.com>
2021-11-07 22:15:16 +0700

    fix rubocop

--- Gemfile
+++ Gemfile
@@ -2,6 +2,6 @@
 
 source 'https://rubygems.org'
 
+gem 'pg'
 gem 'sinatra'
 gem 'sinatra-contrib'
-gem 'pg'
--- app.rb
+++ app.rb
@@ -6,22 +6,20 @@ require 'json'
 require 'byebug'
 require 'pg'
 
-#connect to postgresql
+# connect to postgresql
 memos = []
 begin
-  conection = PG.connect :dbname => 'sinatra_development', :user => 'postgres', :password => ''
+  conection = PG.connect dbname: 'sinatra_development', user: 'postgres', password: ''
 
   t_memos = conection.exec 'SELECT * FROM memos'
 
   t_memos.each do |s_memo|
     memos.push({ id: s_memo['id'], title: s_memo['title'], body: s_memo['body'] })
   end
-
 rescue PG::Error => e
-  val_error = e.message 
-
+  val_error = e.message
 ensure
-  conection.close if conection
+  conection&.close
 end
 
 get '/' do
@@ -39,7 +37,7 @@ end
 # show memo contents
 get '/memos/:id' do
   @title = 'メモを表示'
-  @memo = memos.select{ |s| s[:id] == params[:id] }.first
+  @memo = memos.find { |s| s[:id] == params[:id] }
   erb :show
 end
 
@@ -48,16 +46,16 @@ post '/memos' do
   @title = 'メモを追加'
 
   # create id for new memo_data
-  key_arr = memos.map {|i| i[:id]}
+  key_arr = memos.map { |i| i[:id] }
   last_id = key_arr.max.to_i + 1
 
   begin
-    conection = PG.connect :dbname => 'sinatra_development', :user => 'postgres', :password => ''
+    conection = PG.connect dbname: 'sinatra_development', user: 'postgres', password: ''
     conection.exec "INSERT INTO memos(id, title, body) VALUES (#{last_id}, '#{params[:title]}','#{params[:body]}') ;"
   rescue PG::Error => e
-    val_error = e.message 
+    val_error = e.message
   ensure
-    conection.close if conection
+    conection&.close
   end
 
   redirect '/'
@@ -68,12 +66,12 @@ delete '/memos/:id' do
   @title = 'メモを削除'
 
   begin
-    conection = PG.connect :dbname => 'sinatra_development', :user => 'postgres', :password => ''
+    conection = PG.connect dbname: 'sinatra_development', user: 'postgres', password: ''
     conection.exec "DELETE FROM memos WHERE memos.id = #{params[:id]} ;"
   rescue PG::Error => e
-    val_error = e.message 
+    val_error = e.message
   ensure
-    conection.close if conection
+    conection&.close
   end
 
   redirect '/'
@@ -82,7 +80,7 @@ end
 # to editer page.
 get '/memos/:id/edit' do
   @title = 'メモを修正'
-  @memo = memos.select{ |s| s[:id] == params[:id] }.first
+  @memo = memos.find { |s| s[:id] == params[:id] }
   erb :edit
 end
 
@@ -91,12 +89,12 @@ patch '/memos/:id' do
   @title = 'メモを修正'
 
   begin
-    conection = PG.connect :dbname => 'sinatra_development', :user => 'postgres', :password => ''
+    conection = PG.connect dbname: 'sinatra_development', user: 'postgres', password: ''
     conection.exec "UPDATE memos SET title = '#{params[:title]}', body = '#{params[:body]}' WHERE memos.id = #{params[:id]} ;"
   rescue PG::Error => e
-    val_error = e.message 
+    val_error = e.message
   ensure
-    conection.close if conection
+    conection&.close
   end
 
   redirect '/'
